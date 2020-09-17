7ac54478a89f53b8ba1dbbc64d9ca7b77a229a6b
Aabondd <aabondd@gmail.com>
2020-09-17 22:03:10 +0300

    Updates

---
+++ precache-manifest.e85d13e366e86fa806da15fd6d03dc54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba7cdac9c168186706d52de30a8a97b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b13fc0c768c92e2f8d22",
+    "url": "/goit-react-hw-01-components/static/css/main.a2207663.chunk.css"
+  },
+  {
+    "revision": "13a030a57f3648a9d35e",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13fc0c768c92e2f8d22",
+    "url": "/goit-react-hw-01-components/static/js/main.38d56015.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
